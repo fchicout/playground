adf8a75cdb124174f2f68af5f297c5b0b32ae0dd
Fábio Chicout <fabio.cesar.medeiros@gmail.com>
2012-12-13 20:30:23 -0300

    Added getStudentNames. Not Tested yet

--- rmclassis/script.js
+++ rmclassis/script.js
@@ -1,41 +1,48 @@
 var APP = {
-	_frame: window.frames.CLMain.document,
+    _frame: window.frames.CLMain.document,
 
-		iniciar: function() {
-				jQuery(APP._frame).find("#dgAlunos tr").css('cursor', 'pointer')
-							.toggle(function(event) {
-											if(event.target.tagName != 'INPUT') 
-																jQuery(this).addClass('alunoSelecionado').css('background-color', '#fcc'); 
-																			}, function(event) { 
-																							if(event.target.tagName != 'INPUT')
-																												jQuery(this).removeClass('alunoSelecionado').css('background-color', '') 
-																															});
-																																},
+    iniciar: function () {
+        jQuery(APP._frame).find("#dgAlunos tr").css('cursor', 'pointer')
+                    .toggle(function (event) {
+                        if (event.target.tagName != 'INPUT')
+                            jQuery(this).addClass('alunoSelecionado').css('background-color', '#fcc');
+                    }, function (event) {
+                        if (event.target.tagName != 'INPUT')
+                            jQuery(this).removeClass('alunoSelecionado').css('background-color', '')
+                    });
+    },
 
-																																	aplicarATodos: function() {
-																																			var nota = prompt('Digite a nota');
-																																					if(!nota) return false;
-																																							jQuery(window.frames.CLMain.document)
-																																										.find('input[type=text][name$=Nota]')
-																																													.val(nota);
-																																														
-																																															},
+    aplicarATodos: function () {
+        var nota = prompt('Digite a nota');
+        if (!nota) return false;
+        jQuery(window.frames.CLMain.document)
+                    .find('input[type=text][name$=Nota]')
+                                .val(nota);
 
-																																																aplicarAMarcados: function() {
-																																																		var nota = prompt('Digite a nota');
-																																																				if(!nota) return false;
+    },
 
-																																																						jQuery(window.frames.CLMain.document)
-																																																									.find("#dgAlunos tr.alunoSelecionado")
-																																																												.find("input[type=text][name$=Nota]")
-																																																															.val(nota)
-																																																																		.end().removeClass('alunoSelecionado');
-																																																																			}
-																																																																			};
-																																																																			var element1 = document.createElement("script");
-																																																																			element1.src = "//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js";
-																																																																			element1.type="text/javascript";
-																																																																			element1.onload = function() {
-																																																																				APP.iniciar();
-																																																																				}
-																																																																				document.getElementsByTagName("head")[0].appendChild(element1);
+    aplicarAMarcados: function () {
+        var nota = prompt('Digite a nota');
+        if (!nota) return false;
+
+        jQuery(window.frames.CLMain.document)
+                    .find("#dgAlunos tr.alunoSelecionado")
+                                .find("input[type=text][name$=Nota]")
+                                            .val(nota)
+                                                        .end().removeClass('alunoSelecionado');
+    },
+
+    getStudentNames: function(){
+        jQuery(window.frames.CLMain.document).find("a[href=#]");
+        for(i=0; i < links.length; i++ ){ 
+            console.log(links[i].innerHTML); 
+        }
+    }
+};
+var element1 = document.createElement("script");
+element1.src = "//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js";
+element1.type = "text/javascript";
+element1.onload = function () {
+    APP.iniciar();
+}
+document.getElementsByTagName("head")[0].appendChild(element1);
