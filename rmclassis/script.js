b9ff160ca45003504f6cbfd30d724750cee5445d
Fábio Chicout <fabio.cesar.medeiros@gmail.com>
2012-12-13 19:15:59 -0300

    Commiting the script sent from Dennis Calazans to hack the RMClassis.

---
+++ rmclassis/script.js
@@ -0,0 +1,41 @@
+var APP = {
+	_frame: window.frames.CLMain.document,
+
+		iniciar: function() {
+				jQuery(APP._frame).find("#dgAlunos tr").css('cursor', 'pointer')
+							.toggle(function(event) {
+											if(event.target.tagName != 'INPUT') 
+																jQuery(this).addClass('alunoSelecionado').css('background-color', '#fcc'); 
+																			}, function(event) { 
+																							if(event.target.tagName != 'INPUT')
+																												jQuery(this).removeClass('alunoSelecionado').css('background-color', '') 
+																															});
+																																},
+
+																																	aplicarATodos: function() {
+																																			var nota = prompt('Digite a nota');
+																																					if(!nota) return false;
+																																							jQuery(window.frames.CLMain.document)
+																																										.find('input[type=text][name$=Nota]')
+																																													.val(nota);
+																																														
+																																															},
+
+																																																aplicarAMarcados: function() {
+																																																		var nota = prompt('Digite a nota');
+																																																				if(!nota) return false;
+
+																																																						jQuery(window.frames.CLMain.document)
+																																																									.find("#dgAlunos tr.alunoSelecionado")
+																																																												.find("input[type=text][name$=Nota]")
+																																																															.val(nota)
+																																																																		.end().removeClass('alunoSelecionado');
+																																																																			}
+																																																																			};
+																																																																			var element1 = document.createElement("script");
+																																																																			element1.src = "//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js";
+																																																																			element1.type="text/javascript";
+																																																																			element1.onload = function() {
+																																																																				APP.iniciar();
+																																																																				}
+																																																																				document.getElementsByTagName("head")[0].appendChild(element1);
